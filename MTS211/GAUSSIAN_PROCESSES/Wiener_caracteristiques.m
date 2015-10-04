3e173846418aadb291b4cebd7cf0760c8e51c86f
yfe <yannfeunteun@gmail.com>
2015-10-04 20:21:18 +0200

    Added question related to the caracteristics of the law of a Wiener growing process

---
+++ MTS211/GAUSSIAN_PROCESSES/Wiener_caracteristiques.m
@@ -0,0 +1,33 @@
+% Définition de l'échantillonnage temporel de la trajectoire
+t = 0:0.01:10;
+
+% Coefficient de diffusion du processus
+sigma = 1;
+
+% Nombre de processus à simuler pour le calcul expérimental de l'espérance et de la variance
+nbSim = 50;
+
+% Matrice des processus simulés
+A = zeros(nbSim, length(t));
+
+% Simulations de "nbSim" processus de Wiener
+for i=1:nbSim
+A(i,:) = Wiener(t, sigma);
+end
+
+% f contient les valeurs des espérances pour chaque instant
+f = zeros(length(t));
+% g contient les valeurs des variances pour chaque instant
+f = zeros(length(t));
+
+for i=1:length(t)
+f(i) = mean(A(:,i));
+g(i) = var(A(:,i));
+end;
+
+
+figure;
+plot(t, f, 'r');
+hold on;
+plot(t, g, 'b');
+
