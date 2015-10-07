7d9aa3d8e6b252105c47628ae47a363b4ee1d9a9
yfe <yannfeunteun@gmail.com>
2015-10-07 07:26:58 +0200

    Corrected NZ signal model according to specifications

--- SIT213/etape2/modelisations/rz/getPortionRZ.m
+++ SIT213/etape2/modelisations/rz/getPortionRZ.m
@@ -1,9 +1,11 @@
 function [portion] = getPortionRZ(bit, nSamplesPerBit, amplMin, amplMax)
 
-portion1Length = floor(nSamplesPerBit/2);
-portion2Length = nSamplesPerBit - portion1Length;
+portion1Length = floor(nSamplesPerBit/3);
+portion3Length = portion1Length
+portion2Length = nSamplesPerBit - portion1Length - portion3Length ;
 
-portion1 = ones(1,portion1Length)*((bit==0)*amplMin + (bit==1)*amplMax );
-portion2 = zeros(1,portion2Length);
+portion1 = zeros(1,portion1Length);
+portion2 = ones(1,portion2Length)*((bit==0)*amplMin + (bit==1)*amplMax );
+portion3 = zeros(1,portion3Length);
 
-portion = [portion1 portion2];
+portion = [portion1 portion2 portion3];
