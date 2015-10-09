6e98a10f6d1dde97b4db133fc635eff3cb538486
yfe <yannfeunteun@gmail.com>
2015-10-09 08:03:01 +0200

    Fixed gaussian noise model

--- SIT213/etape3/modelisation/gaussianNoise.m
+++ SIT213/etape3/modelisation/gaussianNoise.m
@@ -2,20 +2,20 @@
 
 % Rappel : SNR : Puissance moyenne du signal / Puissance moyenne du bruit (linéaire) 
 
-x = 0:0.1:1000;
+x = 0:0.1:100000;
 pure = 2.*x+5;
 
 a1 = rand(1,length(pure));
 a2 = rand(1,length(pure));
 
+% Ecart-type
+sigma = 5;
+
 %% Puissance = variance
-P = 500;
-b = P.*sqrt(-2.*log2(1-a1)) .* cos(2*pi.*a2);
+P = sigma^2;
+b = sigma .* sqrt(-2.*log(1-a1)) .* cos(2*pi.*a2);
 
 %plot(pure + b);
-
 histfit(b, 100);
-
-
 %plot(b)
 
