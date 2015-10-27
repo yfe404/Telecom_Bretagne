5908c674c64c16e9130962d4c8e3670f1e842f59
yafeunteun <yannfeunteun@gmail.com>
2015-10-27 11:15:31 +0100

    Added methods Create and Update for service ServiceOffreEmploi

--- INF211/projects/CabinetRecrutement_EJB/ejbModule/eu/telecom_bretagne/cabinet_recrutement/service/IServiceOffreEmploi.java
+++ INF211/projects/CabinetRecrutement_EJB/ejbModule/eu/telecom_bretagne/cabinet_recrutement/service/IServiceOffreEmploi.java
@@ -1,10 +1,14 @@
 package eu.telecom_bretagne.cabinet_recrutement.service;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Set;
 
 import javax.ejb.Remote;
 
+import eu.telecom_bretagne.cabinet_recrutement.data.model.NiveauQualification;
 import eu.telecom_bretagne.cabinet_recrutement.data.model.OffreEmploi;
+import eu.telecom_bretagne.cabinet_recrutement.data.model.SecteurActivite;
 
 @Remote
 public interface IServiceOffreEmploi {
@@ -14,5 +18,12 @@ public interface IServiceOffreEmploi {
 	List<OffreEmploi> listeDesOffresEmploi();
 
 	public void supprimerOffreEmploi(String id);
+	
+	public void ajoutOffreEmploi(String titre, String descriptif, String profilRecherche, NiveauQualification niveauQualificationBean,
+			Set<SecteurActivite> secteurActivites);
+
+	public void miseAJourOffreEmploi(String id, String titre, String descriptif, String profilRecherche, NiveauQualification niveauQualificationBean,
+			Set<SecteurActivite> secteurActivites);
+	
 	
 }
--- INF211/projects/CabinetRecrutement_EJB/ejbModule/eu/telecom_bretagne/cabinet_recrutement/service/ServiceOffreEmploi.java
+++ INF211/projects/CabinetRecrutement_EJB/ejbModule/eu/telecom_bretagne/cabinet_recrutement/service/ServiceOffreEmploi.java
@@ -1,6 +1,8 @@
 package eu.telecom_bretagne.cabinet_recrutement.service;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Set;
 
 import javax.ejb.EJB;
 import javax.ejb.LocalBean;
@@ -8,7 +10,9 @@ import javax.ejb.Stateless;
 
 import eu.telecom_bretagne.cabinet_recrutement.data.dao.OffreEmploiDAO;
 import eu.telecom_bretagne.cabinet_recrutement.data.model.Candidature;
+import eu.telecom_bretagne.cabinet_recrutement.data.model.NiveauQualification;
 import eu.telecom_bretagne.cabinet_recrutement.data.model.OffreEmploi;
+import eu.telecom_bretagne.cabinet_recrutement.data.model.SecteurActivite;
 
 /**
  * Session Bean implementation class ServiceOffreEmploi
@@ -26,13 +30,13 @@ public class ServiceOffreEmploi implements IServiceOffreEmploi
 	public ServiceOffreEmploi() {
 		// Default constructor
 	}
-	
-	
+
+
 	@Override
 	public OffreEmploi getOffreEmploi(int id) {
 		return offreEmploiDAO.findById(id);
 	}
-	
+
 	@Override
 	public List<OffreEmploi> listeDesOffresEmploi() {
 		return offreEmploiDAO.findAll();
@@ -48,36 +52,47 @@ public class ServiceOffreEmploi implements IServiceOffreEmploi
 	}
 
 
-	
-	
-//	public Candidature getCandidature(int id) {
-//		// TODO Auto-generated method stub		
-//		return candidatureDAO.findById(id);
-//	}
-//
-//	@Override
-//	public List<Candidature> listeDesCandidatures() {
-//		return candidatureDAO.findAll();
-//	}
-//
-//	@Override
-//	public void ajoutCandidature(String adresseEmail, String adressePostale,
-//			String cv, Date dateDepot, Date dateNaissance, String nom,
-//			String prenom, NiveauQualification niveauQualificationBean,
-//			Set<SecteurActivite> secteurActivites) {
-//		
-//	}
-//
-//	@Override
-//	public void miseAJourCandidature(String id, String nom, String descriptif,
-//			String adresse_postale) {
-//	}
-//
-//	@Override
-//	public void supprimerCandidature(String id) {
-//		// TODO Auto-generated method stub
-//		
-//	}
+	@Override
+	public void ajoutOffreEmploi(String titre, String descriptionMission,
+			String profilRecherche,
+			NiveauQualification niveauQualificationBean,
+			Set<SecteurActivite> secteurActivites) {
+
+		OffreEmploi offreEmploi = new OffreEmploi();
+
+		offreEmploi.setTitre(titre);
+		offreEmploi.setDescriptionMission(descriptionMission);
+		offreEmploi.setProfilRecherche(profilRecherche);
+		offreEmploi.setNiveauQualificationBean(niveauQualificationBean);
+		offreEmploi.setSecteurActivites(secteurActivites);
+
+
+		offreEmploiDAO.persist(offreEmploi);		
+
+	}
+
+
+	@Override
+	public void miseAJourOffreEmploi(String id, String titre,
+			String descriptionMission, String profilRecherche,
+			NiveauQualification niveauQualificationBean,
+			Set<SecteurActivite> secteurActivites) {
+
+
+		OffreEmploi offreEmploi = offreEmploiDAO.findById(Integer.parseInt(id));
+		if (offreEmploi != null){
+
+			offreEmploi.setTitre(titre);
+			offreEmploi.setDescriptionMission(descriptionMission);
+			offreEmploi.setProfilRecherche(profilRecherche);
+			offreEmploi.setNiveauQualificationBean(niveauQualificationBean);
+			offreEmploi.setSecteurActivites(secteurActivites);
+
+			offreEmploiDAO.update(offreEmploi);
+		}
+
+	}
+
 }
 
 
