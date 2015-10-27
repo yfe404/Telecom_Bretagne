a1bd7568c0879fe6d402e6328a27fd98e7d2cff0
yafeunteun <yannfeunteun@gmail.com>
2015-10-27 12:12:48 +0100

    Added optional services ServiceMessageCandidature and ServiceMessageOffreEmploi

---
+++ INF211/projects/CabinetRecrutement_EJB/ejbModule/eu/telecom_bretagne/cabinet_recrutement/service/ServiceMessageCandidature.java
@@ -0,0 +1,60 @@
+package eu.telecom_bretagne.cabinet_recrutement.service;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.ejb.EJB;
+
+import eu.telecom_bretagne.cabinet_recrutement.data.dao.MessageCandidatureDAO;
+import eu.telecom_bretagne.cabinet_recrutement.data.model.MessageCandidature;
+import eu.telecom_bretagne.cabinet_recrutement.data.model.OffreEmploi;
+
+public class ServiceMessageCandidature implements IServiceMessageCandidature {
+
+	@EJB
+	private MessageCandidatureDAO messageCandidatureDAO;
+
+	public ServiceMessageCandidature() {
+		// Default constructor
+	}
+	
+	@Override
+	public MessageCandidature getMessageCandidature(int id) {
+		return messageCandidatureDAO.findById(id);
+	}
+
+	@Override
+	public List<MessageCandidature> listeDesMessagesCandidature() {
+		return messageCandidatureDAO.findAll();
+	}
+	
+	@Override
+	public void ajoutMessageCandidature(OffreEmploi offreEmploiBean, String corpsMessage, Date dateEnvoi) {
+		MessageCandidature messageCandidature = new MessageCandidature();
+		messageCandidature.setOffreEmploiBean(offreEmploiBean);
+		messageCandidature.setCorpsMessage(corpsMessage);
+		messageCandidature.setDateEnvoi(dateEnvoi);
+		
+		messageCandidatureDAO.persist(messageCandidature);
+	}
+
+	@Override
+	public void miseAJourMessageCandidature(String id, OffreEmploi offreEmploiBean, String corpsMessage, Date dateEnvoi) {
+		MessageCandidature messageCandidature = messageCandidatureDAO.findById(Integer.parseInt(id));
+		if (messageCandidature != null){
+			messageCandidature.setOffreEmploiBean(offreEmploiBean);
+			messageCandidature.setCorpsMessage(corpsMessage);
+			messageCandidature.setDateEnvoi(dateEnvoi);
+			messageCandidatureDAO.update(messageCandidature);
+		}
+		
+	}
+
+	@Override
+	public void supprimerMessageCandidature(String id) {
+		MessageCandidature messCand = messageCandidatureDAO.findById(Integer.parseInt(id));
+		if (messCand != null){
+			messageCandidatureDAO.remove(messCand);
+		}	
+	}
+}
---
+++ INF211/projects/CabinetRecrutement_EJB/ejbModule/eu/telecom_bretagne/cabinet_recrutement/service/ServiceMessageOffreEmploi.java
@@ -0,0 +1,67 @@
+package eu.telecom_bretagne.cabinet_recrutement.service;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.ejb.EJB;
+
+import eu.telecom_bretagne.cabinet_recrutement.data.dao.MessageOffreEmploiDAO;
+import eu.telecom_bretagne.cabinet_recrutement.data.model.Candidature;
+import eu.telecom_bretagne.cabinet_recrutement.data.model.MessageOffreEmploi;
+import eu.telecom_bretagne.cabinet_recrutement.data.model.OffreEmploi;
+
+public class ServiceMessageOffreEmploi implements IServiceMessageOffreEmploi {
+
+
+	@EJB
+	private MessageOffreEmploiDAO messageOffreEmploiDAO;
+
+	public ServiceMessageOffreEmploi() {
+		// Default constructor
+	}
+
+
+	@Override
+	public MessageOffreEmploi getMessageOffreEmploi(int id) {
+		return messageOffreEmploiDAO.findById(id);
+	}
+
+	@Override
+	public List<MessageOffreEmploi> listeDesMessageOffreEmploi() {
+		return messageOffreEmploiDAO.findAll(); 
+	}
+
+	@Override
+	public void ajoutMessageOffreEmploie(OffreEmploi offreEmploiBean, Candidature candidature, String corpsMessage, Date dateEnvoi) {
+		MessageOffreEmploi messageOffreEmploi = new MessageOffreEmploi();
+		messageOffreEmploi.setOffreEmploiBean(offreEmploiBean);
+		messageOffreEmploi.setCandidatureBean(candidature);
+		messageOffreEmploi.setCorpsMessage(corpsMessage);
+		messageOffreEmploi.setDateEnvoi(dateEnvoi);
+
+		messageOffreEmploiDAO.persist(messageOffreEmploi);
+	}
+
+	@Override
+	public void miseAJourMessageOffreEmploi(String id, OffreEmploi offreEmploiBean, Candidature candidature, String corpsMessage, Date dateEnvoi) {
+		MessageOffreEmploi messageOffreEmploi = messageOffreEmploiDAO.findById(Integer.parseInt(id));
+		if (messageOffreEmploi != null){
+			messageOffreEmploi.setOffreEmploiBean(offreEmploiBean);
+			messageOffreEmploi.setCandidatureBean(candidature);
+			messageOffreEmploi.setCorpsMessage(corpsMessage);
+			messageOffreEmploi.setDateEnvoi(dateEnvoi);
+			
+			messageOffreEmploiDAO.update(messageOffreEmploi);
+		}
+
+	}
+
+	@Override
+	public void supprimerMessageOffreEmploi(String id) {
+		MessageOffreEmploi messOE = messageOffreEmploiDAO.findById(Integer.parseInt(id));
+		if (messOE != null){
+			messageOffreEmploiDAO.remove(messOE);
+		}	
+	}
+
+}
